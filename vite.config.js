2bc57f75ff89faa7a49d91c2c2de75e775593339
asmarais <94301506+asmarais@users.noreply.github.com>
2026-01-15 00:06:55 +0100

    Add server configuration to vite.config.js
    Configure Vite server settings with host, port, and allowed hosts.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true,       // binds to 0.0.0.0
+    port: 5173,
+    allowedHosts: [
+      'alb-frontend-1251250178.us-east-1.elb.amazonaws.com', // ALB DNS
+      'localhost',   // optional, for local dev
+      '127.0.0.1'
+    ]}})
